5c7f3c9d38e433c761469548af644a65861e8803
james white jr <jameswhitejr.@jamess-MacBook-Pro-2.local>
2020-08-19 19:13:57 -0400

    fixing form

--- src/shared/Footer.jsx
+++ src/shared/Footer.jsx
@@ -1,18 +1,59 @@
 import React from 'react'
 import '../styles/Footer.css'
 
-const Footer = () => {
+const encode = (data) => {
+  return Object.keys(data)
+      .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+      .join("&");
+}
+
+class Footer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { name: "", email: "", message: "" };
+  }
+
+  handleSubmit = e => {
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({ "form-name": "contact", ...this.state })
+    })
+      .then(() => alert("Success!"))
+      .catch(error => alert(error));
+
+    e.preventDefault();
+  };
+
+  handleChange = e => this.setState({ [e.target.name]: e.target.value });
+
+  render() {
+    const { name, email, message } = this.state;
     return (
-        <div className='footer'>
-    <form name="contact" method="POST" data-netlify="true">
-    <p>Contact Me:</p>
-    <p><label> name: <input type="text" name="name" /> </label></p>
-    <p><label> email: <input type="email" name="email" /></label></p>
-    <p><label> message: <textarea name="message"></textarea></label></p>
-    <button type="submit">Send</button> 
-</form>
-    </div>
-                                )
-                            }
+    <div className='footer'>
+      <form onSubmit={this.handleSubmit}>
+        <p>
+          <label>
+            Your Name: <input type="text" name="name" value={name} onChange={this.handleChange} />
+          </label>
+        </p>
+        <p>
+          <label>
+            Your Email: <input type="email" name="email" value={email} onChange={this.handleChange} />
+          </label>
+        </p>
+        <p>
+          <label>
+            Message: <textarea name="message" value={message} onChange={this.handleChange} />
+          </label>
+        </p>
+        <p>
+          <button type="submit">Send</button>
+        </p>
+      </form>
+      </div>    
+    );
+  }
+}
                             
 export default Footer;
