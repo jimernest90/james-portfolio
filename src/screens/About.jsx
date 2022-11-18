dc57778470a912393c56496f42a39e4d4da47453
James White <jameswhite@jamess-mbp.mynetworksettings.com>
2022-11-18 18:45:27 -0500

    added email

--- src/screens/About.jsx
+++ src/screens/About.jsx
@@ -11,6 +11,8 @@ const About = () =>{
                 <br/>
                 <b>name:</b> 'James',
                 <br/>
+                <b>email:</b> 'Jamwhi101@gmail.com',
+                <br/>
                 <b>occupation:</b> 'Software Engineer',
                 <br/>
                 <b>traits:</b> 'loves working with newer frameworks and libraries to create cool desktop and mobile applications.'
@@ -20,7 +22,7 @@ const About = () =>{
                 </div>
                 &#125; 
                 <div className='functionName'>
-                    <p>const <b>aboutMe</b> &#61; &#40;&#41; &#61;&gt; &#123; console.log	&#40; &#96;Hi I am &#36;&#123; softwareEngineer.name&#125; and I am a &#36;&#123; softwareEngineer.occupation&#125; who &#36;&#123; softwareEngineer.traits&#125; I have experience in &#36;&#123; softwareEngineer.skills&#125;&#96;&#41;
+                    <p>const <b>aboutMe</b> &#61; &#40;&#41; &#61;&gt; &#123; console.log	&#40; &#96;Hi I am &#36;&#123; softwareEngineer.name&#125; and I am a &#36;&#123; softwareEngineer.occupation&#125; who &#36;&#123; softwareEngineer.traits&#125; I have experience in &#36;&#123; softwareEngineer.skills&#125; Contact me at &#36;&#123; softwareEngineer.email&#125;.&#96;&#41;
                     <br/>&#125; </p>
                 </div>
                 <div className='runFunction'>
@@ -28,7 +30,7 @@ const About = () =>{
                 </div>
                 </div>
                 <div className='ranFunction'>
-                Hi I am James and I am a Software Engineer who loves working with newer frameworks and libraries to create cool desktop and mobile applications. I have experience in Javascript, Jquery, Css, Html, React.js, Ruby, Postgresql, Sql, and Ruby on Rails.<div className='symb'>&#124;</div>
+                Hi I am James and I am a Software Engineer who loves working with newer frameworks and libraries to create cool desktop and mobile applications. I have experience in Javascript, Jquery, Css, Html, React.js, Ruby, Postgresql, Sql, and Ruby on Rails. Contact me at Jamwhi101@gmail.com.<div className='symb'>&#124;</div>
                 </div>
         </h4>
     )
